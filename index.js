e6cb2e606454593703ce66edd2c58a8803b953cd
Martin <martin_leiva13@hotmail.com>
2020-04-23 01:15:20 -0300

    Cambios para deployment

--- index.js
+++ index.js
@@ -9,14 +9,9 @@ const app = express();
 conectarDB();
 
 //Habilitar CORS
-app.use(cors());
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
+app.use(cors({
+    origin: 'https://glacial-stream-09387.herokuapp.com/'
+  }));
 
 //Habilitar express.json
 app.use(express.json({ extended: true }));
